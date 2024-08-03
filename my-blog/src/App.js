f172990b8b6f54bdca8277e1c0233a5907cf79dc
Dylan Corr <dylancorr.g@gmail.com>
2024-08-03 12:36:03 +0100

    my second commit

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Dylan's Blog</h1> {/* Title for Dylan's Blog */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Removed the "Edit src/App.js and save to reload" message as it's not relevant to a blog */}
         </p>
         <a
           className="App-link"
